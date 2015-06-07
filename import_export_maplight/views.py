910b25c66f15aabeab9f63ebdf473403aff4580e
Dale John McGrew <dale.mcgrew@wevoteusa.org>
2015-06-07 13:06:41 -0700

    Starting framework for importing Maplight for a San Francisco Ballot.

--- import_export_maplight/views.py
+++ import_export_maplight/views.py
@@ -2,6 +2,22 @@
 # Brought to you by We Vote. Be good.
 # -*- coding: UTF-8 -*-
 
-from django.shortcuts import render
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from import_export_maplight.models import import_maplight_from_json_view
 
-# Create your views here.
+def import_maplight_from_json_view(request):
+    """
+    Take data from Test XML file and store in the local Voting Info Project database
+    """
+    # If person isn't signed in, we don't want to let them visit this page yet
+    if not request.user.is_authenticated():
+        return redirect('/admin')
+
+    import_maplight_from_json_sample_files()
+
+    messages.add_message(request, messages.INFO, 'Maplight sample data imported.')
+
+    return HttpResponseRedirect(reverse('import_export:import_export_index', args=()))
