f5b885f336c019358186c1f3f726f4ad29658631
Dale John McGrew <dale.mcgrew@wevoteusa.org>
2015-08-16 12:03:04 -0700

    Moved developer specific configurations for local.py into environment_variables-template.json file.

--- wevotebase/local.py
+++ wevotebase/local.py
@@ -15,17 +15,13 @@ DEBUG = True
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'WeVoteDB',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',  # localhost
-        'PORT': '',  # 5432
+        'ENGINE': get_environment_variable('DATABASE_ENGINE'),
+        'NAME': get_environment_variable('DATABASE_NAME'),
+        'USER': get_environment_variable('DATABASE_USER'),
+        'PASSWORD': get_environment_variable('DATABASE_PASSWORD'),
+        'HOST': get_environment_variable('DATABASE_HOST'),  # localhost
+        'PORT': get_environment_variable('DATABASE_PORT'),  # 5432
     }
 }
 
@@ -36,6 +32,6 @@ DATABASES = {
 #   LOG_STREAM_LEVEL    Integer     Log level of stream handler: CRITICAL, ERROR, INFO, WARN, DEBUG
 #   LOG_FILE_LEVEL      Integer     Log level of file handler: CRITICAL, ERROR, INFO, WARN, DEBUG
 LOG_STREAM = True
-LOG_FILE = "/var/log/wevote/wevote.log"
+LOG_FILE = get_environment_variable('LOG_FILE')
 LOG_STREAM_LEVEL = logging.DEBUG
 LOG_FILE_LEVEL = logging.ERROR
